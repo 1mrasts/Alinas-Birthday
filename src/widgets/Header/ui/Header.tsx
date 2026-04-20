dd9a2b3fde0d75313e22e6bd916738776b2cb078
1mrasts <olgaakshidi.ru@gmail.com>
2026-04-20 22:07:52 +0300

    refactor: extract header container and update styles

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router'
 import s from './Header.module.scss'
 
 export const Header = () => {
-	return <header className={s.header}></header>
+	return (
+		<header className='wrapper'>
+			<div className={s.container}>
+				<nav>
+					<Link to='/about-me'>Обо мне</Link>
+					<Link to='/family'>Семья</Link>
+					<Link to='/childhood'>Детство</Link>
+				</nav>
+			</div>
+		</header>
+	)
 }
